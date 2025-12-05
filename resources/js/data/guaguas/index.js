a1113ba9ed106dcc1fa4d346ad9228e4ceb299b0
DevOrtega <carloso2103@gmail.com>
2025-12-05 14:26:42 +0000

    refactor(guaguas): extract data to dedicated modules
    - Extract bus stop coordinates to data/guaguas/paradas.js
    - Extract configuration constants to data/guaguas/config.js
    - Extract route definitions to data/guaguas/routes.js
    - Add barrel export in data/guaguas/index.js

---
+++ resources/js/data/guaguas/index.js
@@ -0,0 +1,12 @@
+/**
+ * Export all guaguas data
+ */
+export { PARADAS, getParada } from './paradas.js';
+export { 
+  MAIN_LINES, 
+  BUS_LINES, 
+  COMPANY_COLORS, 
+  MAP_CONFIG, 
+  SIMULATION_CONFIG 
+} from './config.js';
+export { getRoutes } from './routes.js';
